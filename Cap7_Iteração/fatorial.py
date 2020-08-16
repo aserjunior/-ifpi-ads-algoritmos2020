a5dfec3a35dcd8fd393357907e2df001c4d83c2a
Aser Junior <61999214+aserjunior@users.noreply.github.com>
2020-08-16 19:57:32 -0300

    Update fatorial.py

--- Cap7_Iteração/fatorial.py
+++ Cap7_Iteração/fatorial.py
@@ -1,10 +1,15 @@
 def main():
     numero = int(input('Escolha um número: '))
+    
+    
     valor = numero - 1
 
+    
     while valor - 1 > 0:
         numero = numero * valor
         valor = valor - 1
+    
+    
     print(numero)
 
 
