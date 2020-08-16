4f698ce52f07ac771608d6822a0a11060edfd7f4
Aser Junior <61999214+aserjunior@users.noreply.github.com>
2020-08-16 17:03:05 -0300

    Update PG.py

--- Cap7_Iteração/PG.py
+++ Cap7_Iteração/PG.py
@@ -1,4 +1,3 @@
-#Leia as variáveis A0, Limite e R e escreva os valores menores que Limite gerados pela Progressão Geométrica que tem por valor inicial A0 e razão R.
 def main():
     valor_inicial = int(input('Valor inicial: '))
     limite = int(input('Limite: '))
