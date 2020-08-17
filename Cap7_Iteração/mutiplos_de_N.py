ea6c2c42970b23d0b38f69f77d71e2e68c5a7eaa
Aser Junior <61999214+aserjunior@users.noreply.github.com>
2020-08-17 20:17:03 -0300

    Add files via upload

---
+++ Cap7_Iteração/mutiplos_de_N.py
@@ -0,0 +1,21 @@
+def main():
+    n = int(input('Número: '))
+    limite_inferior = int(input('Limite Inferior: '))
+    limite_superior = int(input('Limite Superior: '))
+
+
+    multiplos = limite_inferior
+
+
+    while multiplos < limite_superior:
+        if multiplos % n == 0:
+            print(multiplos)
+
+
+        multiplos = multiplos + 1
+
+
+    print('>>> Esses são os múltiplos de {} que estão entre {} e {}'.format(n, limite_inferior, limite_superior))
+
+
+main()
