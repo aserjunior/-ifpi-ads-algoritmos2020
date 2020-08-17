9bf3154d35b509f2c0623c8b085cf4bd3f41958d
Aser Junior <61999214+aserjunior@users.noreply.github.com>
2020-08-17 01:27:51 -0300

    Add files via upload

---
+++ Cap7_Iteração/fatorial.py
@@ -0,0 +1,16 @@
+def main():
+    numero = int(input('Escolha um número: '))
+    inicio = numero
+
+    valor = numero - 1
+
+
+    while valor - 1 > 0:
+        numero = numero * valor
+        valor = valor - 1
+
+
+    print('>>> O fatorial de {} é {}'.format(inicio, numero))
+
+
+main()
