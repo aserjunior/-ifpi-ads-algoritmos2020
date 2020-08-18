2c2a17d13c28271ec58e801f8b1f7bde626685a6
Aser Junior <61999214+aserjunior@users.noreply.github.com>
2020-08-18 00:02:33 -0300

    Add files via upload

---
+++ Cap7_Iteração/raiz_quadrada.py
@@ -0,0 +1,23 @@
+def main():
+    n = int(input('Escolha um número: '))
+    numero = n
+    inicio = 1
+    raiz = 0
+
+
+    while inicio < numero:
+        valor = inicio ** 0.5
+        if valor % 1 == 0:
+            raiz = valor
+
+
+        inicio = inicio + 1
+
+
+        quadrado = raiz ** 2
+
+
+    print('>>> O quadrado mais proximo de {} é {} de raiz {}'.format(n, quadrado, raiz))
+
+
+main()
